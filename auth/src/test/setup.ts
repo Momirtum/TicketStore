fb593ae6935937c9cef14590d66a7cf4e9b45a27
Emir Taştan <emirt398@gmail.com>
2022-03-28 19:08:27 +0300

    Finished tested

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -26,3 +26,24 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
