532bea02e4077c5f4c974502b3c473b6c3daa641
Emir Taştan <emirt398@gmail.com>
2022-03-30 22:45:45 +0300

    Client created
    Skaffold and ingress-srv files changed.
    Dockerfile , client-depl ,  .dockerignore and next.config created in client folder.
    _app.js and index.js created in pages folder.
    Created  auth folder in pages and added signup.js

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -14,8 +14,8 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
-      .isLength({ min: 8, max: 20 })
-      .withMessage("Password must be between 8 and 20 characters"),
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
